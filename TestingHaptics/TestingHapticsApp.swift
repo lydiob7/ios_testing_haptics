6b25ff8da0ddd2f94f56879678657c77f463a362
lydiob7 <lydiob7@tutanota.com>
2024-08-08 08:01:02 -0300

    Initial Commit

---
+++ TestingHaptics/TestingHapticsApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestingHapticsApp.swift
+//  TestingHaptics
+//
+//  Created by Tomi Scattini on 08/08/2024.
+//
+
+import SwiftUI
+
+@main
+struct TestingHapticsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
