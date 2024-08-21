5f50170bb742e4146ec266d0a292ec78aff2bef9
lydiob7 <lydiob7@tutanota.com>
2024-08-21 07:58:58 -0300

    Testing haptics

--- TestingHaptics/ContentView.swift
+++ TestingHaptics/ContentView.swift
@@ -5,17 +5,47 @@
 //  Created by Tomi Scattini on 08/08/2024.
 //
 
+import CoreHaptics
 import SwiftUI
 
 struct ContentView: View {
+    @State private var engine: CHHapticEngine?
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        Button("Tap me") {
+            complexSuccess()
         }
-        .padding()
+        .onAppear(perform: prepareHaptics)
+    }
+    
+    func prepareHaptics() {
+        guard CHHapticEngine.capabilitiesForHardware().supportsHaptics else { return }
+        
+        do {
+            engine = try CHHapticEngine()
+            try engine?.start()
+        } catch {
+            print("There was an error creating the engine: \(error.localizedDescription)")
+        }
+    }
+    
+    func complexSuccess() {
+        guard CHHapticEngine.capabilitiesForHardware().supportsHaptics else { return }
+        var events = [CHHapticEvent]()
+        
+        let intensity = CHHapticEventParameter(parameterID: .hapticIntensity, value: 1)
+        let sharpness = CHHapticEventParameter(parameterID: .hapticSharpness, value: 0)
+        let event = CHHapticEvent(eventType: .hapticTransient, parameters: [intensity, sharpness], relativeTime: 0)
+        events.append(event)
+        
+        do {
+            let pattern = try CHHapticPattern(events: events, parameters: [])
+            let player = try engine?.makePlayer(with: pattern)
+            try player?.start(atTime: 0)
+        } catch {
+            print("Failed to play pattern: \(error.localizedDescription)")
+        }
+        
     }
 }
 
